006c6de9735eacb1c39c38d89bacffe8b5892606
Loïs <lois_m@outlook.com>
2021-11-14 00:29:11 +0100

    add middleware

--- server.js
+++ server.js
@@ -3,10 +3,59 @@ const app = require('./app');
 
 const cors = require("cors");
 
-app.set("port", process.env.PORT || 3000);
+//la fonction normalizePort renvoie un port valide, 
+//qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+//la fonction errorHandler recherche les différentes 
+//erreurs et les gère de manière appropriée.
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  //Elle est ensuite enregistrée dans le serveur
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+//un écouteur d'évènements est également enregistré, consignant le port ou 
+//le canal nommé sur lequel le serveur s'exécute dans la console.
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+server.listen(port);
+
 
 var corsOptions = {
   origin: "http://localhost:8081",
