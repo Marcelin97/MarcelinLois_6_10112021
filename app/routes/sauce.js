aa3c04537195697a181d6f60f0d305b0d71c3a1b
Loïs <lois_m@outlook.com>
2021-11-25 15:36:48 +0100

    add put routes and update post route

--- app/routes/sauce.js
+++ app/routes/sauce.js
@@ -6,11 +6,17 @@ const sauceCtrl = require("../controllers/sauce")
 //middleware d'authentification que nous appliquerons à nos routes pour les protégés
 const auth = require("../middleware/auth");
 
+//POST
+router.post("/", sauceCtrl.createSauce);
+
+//PUT
+router.put("/:id", sauceCtrl.modifySauce);
+
 //GET pour un seul objet
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 
-//GET
-router.get("/", auth, sauceCtrl.getAllSauce);
+//GET ALL
+router.get("/", auth, sauceCtrl.getAllSauces);
 
 //on exporte le router de ce fichier
 module.exports = router;
