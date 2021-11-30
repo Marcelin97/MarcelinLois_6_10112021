902f391766e8a7e058e77aacaca28a9319470dc9
Loïs <lois_m@outlook.com>
2021-11-30 13:53:51 +0100

    add like route in folder controllers

--- app/controllers/sauce.js
+++ app/controllers/sauce.js
@@ -1,4 +1,4 @@
-const Sauce = require("../models/sauce")
+const Sauce = require("../models/sauce");
 
 //POST
 exports.createSauce = (req, res, next) => {
@@ -17,7 +17,7 @@ exports.createSauce = (req, res, next) => {
 
 //PUT
 exports.modifySauce = (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id})
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => {
       res.status(200).json({
         message: "Sauce mise à jour avec succès !!",
@@ -59,9 +59,9 @@ exports.deleteSauce = (req, res, next) => {
 
 //GET ONE
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
 };
 
 //GET ALL
@@ -71,3 +71,50 @@ exports.getAllSauces = (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
+// //LIKE || DISLIKE
+// exports.likeOneSauce = (req, res, next) => {
+//   Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+//     if (req.body.like === 1 && !usersLiked.includes(req.body.userId)) {
+//       sauce
+//         .updateOne({ _id: req.params.id })
+//         .then(() => res.status(200).json({ message: "sauce liked" }))
+//         .catch((error) => res.status(400).json({ error }));
+//     }
+//   });
+// };
+
+//LIKE || DISLIKE
+exports.likeSauce = (req, res, next) => {
+  // Params
+  let userId = req.body.userId;
+  let like = req.body.like;
+
+  //on vérifie que la sauce existe bien
+  if (!sauce) {
+    res.status(404).json({
+      error: new Error("Cette sauce n'existe pas !"),
+    });
+  }
+
+  Sauce.findById(req.params.id).then(async (sauce) => {
+    // Like
+    //If user like
+    switch (parseInt(like)) {
+      case 1:
+        if (sauce.usersDisliked.includes(userId)) {
+          sauce.likes--
+          sauce.usersDisliked.splice(
+            sauce.usersDisliked.indexOf(userId),
+            1,
+          );
+        }
+        sauce.usersLiked.push(userId);
+        sauce.likes++
+        break;
+      case 0:
+        break;
+      case -1:
+        break;
+    }
+  });
+};
